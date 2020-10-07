a3033bf74a08444e3bb063ae8c33963540f37f8e
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-10-08 02:28:29 +0400

    ⚙️ updated test scripts

--- javascript/src/_config/pre.js
+++ javascript/src/_config/pre.js
@@ -2,4 +2,10 @@ const fse = require("fs-extra");
 const path = require("path");
 
 const mainJsonPath = path.join(__dirname, "../_data/main.json");
-if (!fse.pathExistsSync(mainJsonPath)) fse.writeJSON(mainJsonPath, {});
+if (!fse.pathExistsSync(mainJsonPath)) {
+  console.log("creating placeholder file main.json");
+  fse.ensureFileSync(mainJsonPath);
+  fse.writeJSON(mainJsonPath, {});
+} else {
+  console.log("main.json exists");
+}
