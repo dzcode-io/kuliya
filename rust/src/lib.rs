eeec53c6817dc3b1165a328acd9f6aa79bfa3ab8
omdxp <omarbelghaouti@gmail.com>
2024-01-02 23:26:49 +0100

    chore: impl Display for all fields

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 static DATA_FOLDER: &'static str = "src/_data";
 static CARGO_MANIFEST_DIR: &'static str = env!("CARGO_MANIFEST_DIR");
@@ -10,6 +11,13 @@ pub struct Name {
     pub ar: String,
     pub en: String,
     pub fr: String,
+}
+
+impl std::fmt::Display for Name {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let obj = json!({"ar": self.ar, "en": self.en, "fr": self.fr});
+        write!(f, "{}", serde_json::to_string_pretty(&obj).unwrap())
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
@@ -32,11 +40,33 @@ pub enum Type {
     Sector,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+impl std::fmt::Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Type::University => write!(f, "UNIVERSITY"),
+            Type::Academy => write!(f, "ACADEMY"),
+            Type::PrivateSchool => write!(f, "PRIVATE_SCHOOL"),
+            Type::Institute => write!(f, "INSTITUTE"),
+            Type::Faculty => write!(f, "FACULTY"),
+            Type::Department => write!(f, "DEPARTMENT"),
+            Type::Specialty => write!(f, "SPECIALTY"),
+            Type::Sector => write!(f, "SECTOR"),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct Terms {
     #[serde(rename = "perYear")]
     pub per_year: usize,
     pub slots: Vec<usize>,
+}
+
+impl std::fmt::Display for Terms {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let obj = json!({"perYear": self.per_year, "slots": self.slots});
+        write!(f, "{}", serde_json::to_string_pretty(&obj).unwrap())
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -45,6 +75,25 @@ pub struct Schema {
     #[serde(rename = "type")]
     pub ty: Type,
     pub terms: Option<Terms>,
+}
+
+impl std::fmt::Display for Schema {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.terms.is_some() {
+            let obj = json!({
+                "name": self.name,
+                "type": self.ty,
+                "terms": self.terms.clone().unwrap()
+            });
+            return write!(f, "{}", serde_json::to_string_pretty(&obj).unwrap());
+        } else {
+            let obj = json!({
+                "name": self.name,
+                "type": self.ty
+            });
+            return write!(f, "{}", serde_json::to_string_pretty(&obj).unwrap());
+        }
+    }
 }
 
 pub fn get_node_by_path(path: &str) -> Option<Schema> {
