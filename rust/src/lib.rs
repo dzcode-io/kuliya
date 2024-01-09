ed836fe003416746883ad88d959ffa6c41d3b777
Zakaria Mansouri <zakman.dev@gmail.com>
2024-01-09 21:16:20 +0100

    added simple crate level doc

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,26 @@
+#![warn(missing_docs)]
+
+//! Algeria's college hierarchy dataset as a crate with useful APIs.
+//!
+//! Example
+//!
+//! ```rust
+//! use kuliya::get_node_by_path;
+//!
+//! let faculty_of_science_and_technology = get_node_by_path("umkb/fst");
+//!
+//! assert_eq!(faculty_of_science_and_technology.is_some(), true);
+//!
+//! let faculty_of_science_and_technology = faculty_of_science_and_technology.unwrap();
+//!
+//! assert_eq!(faculty_of_science_and_technology.name.ar, "كلية العلوم والتكنلوجيا");
+//!
+//! // when the path is not found, the function returns None
+//! let non_existing_node = get_node_by_path("umkb/fst/unknown");
+//!
+//! assert_eq!(non_existing_node.is_none(), true);
+//! ```
+
 mod _auto_generated;
 mod api;
 mod node;
