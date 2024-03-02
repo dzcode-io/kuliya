c90ee1f7d700e94d4692fbf16313a34b30c8c02e
omdxp <omarbelghaouti@gmail.com>
2024-03-02 12:25:23 +0100

    add kuliya header file

---
+++ c/kuliya.h
@@ -0,0 +1,19 @@
+#ifndef KULIYA_H
+#define KULIYA_H
+
+#define KULIYA_API extern
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    KULIYA_API int get_node_by_path(const char *path)
+    {
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
