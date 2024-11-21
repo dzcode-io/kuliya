008608356c6fffa234ba0bdcfe662b64f3ffb0aa
Zakaria Mansouri <zakman.dev@gmail.com>
2024-11-21 14:43:17 +0100

    chore: nest struct definition using nest_struct crate

--- rust/src/static/node/mod.rs
+++ rust/src/static/node/mod.rs
@@ -1,47 +1,38 @@
+use nest_struct::nest_struct;
 #[cfg(feature = "serde_derive")]
 use serde::Serialize;
 
-#[derive(Debug, PartialEq)]
-#[cfg_attr(feature = "serde_derive", derive(Serialize))]
-pub struct NodeName {
-    pub ar: &'static str,
-    pub en: &'static str,
-    pub fr: &'static str,
-}
-
-#[derive(Debug, PartialEq)]
-#[cfg_attr(
-    feature = "serde_derive",
-    derive(Serialize),
-    serde(tag = "type"),
-    serde(rename_all = "UPPERCASE")
-)]
-pub enum NodeType {
-    University,
-    Academy,
-    PrivateSchool,
-    Institute,
-    Faculty,
-    Department,
-    Specialty { terms: NodeTerms },
-    Sector { terms: NodeTerms },
-}
-
-#[derive(Debug, PartialEq, Clone)]
-#[cfg_attr(
-    feature = "serde_derive",
-    derive(Serialize),
-    serde(rename_all = "camelCase")
-)]
-pub struct NodeTerms {
-    pub per_year: usize,
-    pub slots: &'static [i32],
-}
-
+#[nest_struct]
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "serde_derive", derive(Serialize))]
 pub struct Node {
-    pub name: NodeName,
+    pub name: nest! {
+        pub ar: &'static str,
+        pub en: &'static str,
+        pub fr: &'static str,
+    },
     #[cfg_attr(feature = "serde_derive", serde(flatten))]
-    pub r#type: NodeType,
+    pub r#type: nest! {
+        #[derive(Debug, PartialEq)]
+        #[cfg_attr(feature = "serde_derive", derive(Serialize), serde(tag = "type"), serde(rename_all = "UPPERCASE"))]
+        pub enum NodeType {
+            University,
+            Academy,
+            PrivateSchool,
+            Institute,
+            Faculty,
+            Department,
+            Specialty {
+                terms: nest! {
+                    #[derive(Debug, PartialEq, Clone)]
+                    #[cfg_attr(feature = "serde_derive", derive(Serialize), serde(rename_all = "camelCase"))]
+                    pub struct NodeTerms {
+                        pub per_year: usize,
+                        pub slots: &'static [i32],
+                    }
+                }
+            },
+            Sector { terms: NodeTerms },
+        }
+    },
 }
